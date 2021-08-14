5a4f1163c51bcfa16ccb7894257a32337dddcbeb
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-08-14 16:04:07 -0300

    Se agregaron actualizaciones

--- src/Components/Contactos/Contactos.js
+++ src/Components/Contactos/Contactos.js
@@ -1,43 +1,37 @@
 import React, {useState} from "react";
+import emailjs from 'emailjs-com';
 
 function Contact() {
 
-    const [prueba, setPrueba] = useState(JSON.parse(localStorage.getItem('contactos')) || []);
-    localStorage.setItem('account', JSON.stringify(prueba)); 
-
     /*Listado de Usuarios*/
     const [newAccount, setNewAccount] = useState({
+        name: '',
         email: '',
-        password: '',
     });
+
+    (function(){
+        emailjs.init("user_c4fhNwUTWdvy2KRvMtgc6");
+     })();
     
     /*Setea */
     const setAccount = (event) =>{
         setNewAccount({...newAccount, [event.target.name]: event.target.value});
     };
 
-    const accounts = () => {
-        if(newAccount.email != "" && newAccount.password != ""){
-            setPrueba([...prueba, newAccount]);
-            localStorage.setItem('contactos', JSON.stringify(prueba));
-            setNewAccount({
-                email: '',
-                password: '',
-            });
-        }else{
-            alert("Complete los campos")
-        }
-    };
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.send('service_1wb82sv', 'template_vylzzko', {email: e.target.email.value, name: e.target.name.value});
+    }
 
     return(
-        <form className="d-flex flex-column align-items-center form-contact justify-content-center mt-5">
-            <label htmlFor="" >Email</label>
-            <input type="text" name="email" className="w-25" value={newAccount.email} onChange={setAccount}/>
+        <form className="d-flex flex-column align-items-center form-contact justify-content-center mt-5" onSubmit={sendEmail}>
+            <label htmlFor="" >Name</label>
+            <input type="text" name="name" className="w-25" value={newAccount.name} onChange={setAccount}/>
 
-            <label htmlFor="">Password</label>
-            <input type="password" name="password" className="w-25" value={newAccount.password} onChange={setAccount}/>
+            <label htmlFor="">Email</label>
+            <input type="email" name="email" className="w-25" value={newAccount.email} onChange={setAccount}/>
 
-            <a className="btn btn-primary w-25 mt-3 mb-5" onClick={accounts}>Add</a>                
+            <input type="submit" className="btn btn-primary w-25 mt-3 mb-5" value="Add"/>               
         </form>
     )
 }
